7e54548ce465bc1668875560760e7902f4947def
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-10-30 12:35:00 -0700

    commit tree 5c46166585ba6c53b046ab044f54887f244ddb60

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindrome = reverseWord(word)
+  
+  return palindrome === word
 }
 
+function reverseWord(word){
+  let splitWord = word.split("")
+  let reverseWord = splitWord.reverse()
+  return reverseWord.join("")
+}
 /* 
   Add your pseudocode here
+  splits input into individual array elements
+  reverses the array
+  joins individual elements into single string
+  compares new string to original inputed word
+  returns true if variables are the same
+  returns false if variables are different
 */
 
 /*
   Add written explanation of your solution here
+  .split() creates individual elements for each character in the string
+  .reverse() reverses the array
+  .join puts each element in the array into new array of one string
 */
 
 // You can run `node index.js` to view these console logs
